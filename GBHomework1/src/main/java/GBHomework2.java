83580bee9a543f09032269e36f5693d825cf030d
Alvi <huskartp@gmail.com>
2021-10-13 23:54:39 +0300

    Second homework

--- GBHomework1/src/main/java/GBHomework2.java
+++ GBHomework1/src/main/java/GBHomework2.java
@@ -34,7 +34,7 @@ public class GBHomework2 {
     }
 
     public static boolean isLeapYear(int year){
-        if(year % 2 == 0 && year % 400 == 0){
+        if(year % 4 == 0 && year % 400 == 0){
             return true;
         }else if(year % 100 == 0){
             return false;
