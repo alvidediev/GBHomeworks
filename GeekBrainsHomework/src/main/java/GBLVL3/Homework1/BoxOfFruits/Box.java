86e053bc64130bd5e3abe33b3d56af758977c70b
Alvi <huskartp@gmail.com>
2022-02-10 11:19:34 +0300

    Doing some work on project

--- GeekBrainsHomework/src/main/java/GBLVL3/Homework1/BoxOfFruits/Box.java
+++ GeekBrainsHomework/src/main/java/GBLVL3/Homework1/BoxOfFruits/Box.java
@@ -8,12 +8,12 @@ public class Box<T extends Fruit> {
     List<T> fruits;
 
     //Коробка ArrayList
-    public Box(T fruits) {
+    public Box(T... fruits) {
         this.fruits = new ArrayList<>(Arrays.asList(fruits));
     }
 
     //Вес коробки
-    private float getWeight() {
+    public float getWeight() {
         float totalWeightOfBox = 0f;
         for (T fruit : fruits) {
             totalWeightOfBox = totalWeightOfBox + fruit.weight;
@@ -25,12 +25,17 @@ public class Box<T extends Fruit> {
      * Моя уверенность в данном ДЗ окончательно пошатнулась на этом моменте. Если этот велосипедный завод каким то
      * чудом окажется правильным, то просьба подсказать как сделать получше...
      */
-    private boolean compare(Box<? extends Fruit> anotherBox) {
+    public boolean compare(Box<? extends Fruit> anotherBox) {
         return getWeight() == anotherBox.getWeight();
     }
 
+    public void replaceFruitsToAnotherBox(Box<T> anotherBox){
+        anotherBox.fruits.addAll(this.fruits);
+        this.fruits.clear();
+    }
+
     //добавить фрукты в корзину
-    private void addFruits(T fruit){
+    public void addFruits(T fruit){
         fruits.add(fruit);
     }
 }
